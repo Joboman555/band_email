b8e2cf217e1918e3e469e175b874510c30cbf2b5
Jonah Spear <Jonahspear512@gmail.com>
2019-09-21 20:09:17 -0400

    build list of concerts

--- scrape_band_names.py
+++ scrape_band_names.py
@@ -2,16 +2,27 @@
 
 from simple_get import simple_get
 from bs4 import BeautifulSoup
+from collections import namedtuple
+from datetime import datetime
 
+Concert = namedtuple('Concert', ['band_name', 'date', 'venue'])
+concerts = []
 
 # 1. Get band names performing in sinclair in October
+# Connect to Sinclair Website
 raw_html = simple_get('https://www.sinclaircambridge.com/events')
 html = BeautifulSoup(raw_html, 'html.parser')
 
+
+
+# Parse through entries
 for entry in html.find_all("div", class_="entry"):
     info = entry.find("div", class_='info')
     band_name = info.find("div", class_='title').h3.text.strip()
     date = info.find("div", class_='date-time-container').find("span", class_='date').text.strip()
-    print(band_name, date)
 
-# 2. Build data structure with band name, date of performance, venue
+    # Add parsed entry to set of concerts
+    concerts.append(Concert(band_name, date, 'The Sinclair'))
+
+for c in concerts:
+    print(c)
