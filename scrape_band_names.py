e66a26449d0ba56ef0140bb92a44436f563d3a30
Jonah Spear <Jonahspear512@gmail.com>
2019-09-21 20:01:19 -0400

    Get Band Names and concert dates

--- scrape_band_names.py
+++ scrape_band_names.py
@@ -7,6 +7,11 @@ from bs4 import BeautifulSoup
 # 1. Get band names performing in sinclair in October
 raw_html = simple_get('https://www.sinclaircambridge.com/events')
 html = BeautifulSoup(raw_html, 'html.parser')
-print(html)
+
+for entry in html.find_all("div", class_="entry"):
+    info = entry.find("div", class_='info')
+    band_name = info.find("div", class_='title').h3.text.strip()
+    date = info.find("div", class_='date-time-container').find("span", class_='date').text.strip()
+    print(band_name, date)
 
 # 2. Build data structure with band name, date of performance, venue
