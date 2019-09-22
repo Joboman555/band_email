148ae3deec3c61e48e07ca1349ef7b9dba92cd63
Jonah Spear <Jonahspear512@gmail.com>
2019-09-21 20:30:21 -0400

    extract scrape_sinclair fn

--- scrape_band_names.py
+++ scrape_band_names.py
@@ -9,25 +9,26 @@ ConcertInfo = namedtuple('ConcertInfo', ['date', 'venue'])
 concerts = {}
 
 # 1. Get band names performing in sinclair in October
-# Connect to Sinclair Website
-raw_html = simple_get('https://www.sinclaircambridge.com/events')
-html = BeautifulSoup(raw_html, 'html.parser')
+def scrape_sinclair(month):
+    # Connect to Sinclair Website
+    html = BeautifulSoup(simple_get('https://www.sinclaircambridge.com/events'))
 
-# Parse through entries
-for entry in html.find_all("div", class_="entry"):
-    info = entry.find("div", class_='info')
-    band_name = info.find("div", class_='title').h3.text.strip()
-    date_str = info.find("div", class_='date-time-container').find("span", class_='date').text.strip()
+    # Parse through entries
+    for entry in html.find_all("div", class_="entry"):
+        info = entry.find("div", class_='info')
+        band_name = info.find("div", class_='title').h3.text.strip()
+        date_str = info.find("div", class_='date-time-container').find("span", class_='date').text.strip()
 
-    # Convert date string to datetime object
-    date = datetime.strptime(' '.join(date_str.split()[1:]), '%b %d, %Y')
+        # Convert date string to datetime object
+        date = datetime.strptime(' '.join(date_str.split()[1:]), '%b %d, %Y')
 
-    # Add parsed entry to dict of concerts
-    if band_name not in concerts:
-        # only add concerts in October
-        if date.month == 10:
-            concerts[band_name] = ConcertInfo(date, 'The Sinclair')
+        # Add parsed entry to dict of concerts
+        if band_name not in concerts:
+            # only add concerts in October
+            if date.month == month:
+                concerts[band_name] = ConcertInfo(date, 'The Sinclair')
 
+scrape_sinclair(month=10)
 
 for k in concerts:
     print(k, concerts[k])
